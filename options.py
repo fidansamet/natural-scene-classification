777aac6cd1720ce66dbba7fb0b4cd27bda4acd96
fidansamet <fidansamet@hacettepe.edu.tr>
2021-05-01 01:47:43 +0300

    new options added

--- options.py
+++ options.py
@@ -4,15 +4,17 @@ import argparse
 class Options:
     def initialize(self, parser):
         parser.add_argument('-data_path', type=str, default='./data/', help='path to dataset')
+        parser.add_argument('-vgg19', action='store_true', help='if specified, use VGG19 features')
 
         # train
-        parser.add_argument('-model_name', type=str, default='sl_nn_', help='name of the model to save')
+        parser.add_argument('-model_name', type=str, default='model.txt', help='name of the model to save')
         parser.add_argument('-hidden_layer_num', type=int, default=1, help='number of hidden layers')
-        parser.add_argument('-hidden_unit_num', type=int, default=1, help='number of hidden units in hidden layers')
+        parser.add_argument('-hidden_unit_num', type=int, default=300, help='number of hidden units in hidden layers')
+        parser.add_argument('-epoch_num', type=int, default=50, help='number of epochs')
         parser.add_argument('-batch_size', type=int, default=32, help='batch size for mini-batch gradient descent')
-        parser.add_argument('-learning_rate', type=float, default=0.01, help='learning rate for gradient descent')
+        parser.add_argument('-learning_rate', type=float, default=0.1, help='learning rate for gradient descent')
         parser.add_argument('-reduce_lr', action='store_true', help='if specified, reduce learning rate')
-        parser.add_argument('-activation_func', type=str, default='sigmoid', help='sigmoid | tanh| relu')
+        parser.add_argument('-activation_func', type=str, default='relu', help='sigmoid | tanh | relu')
         parser.add_argument('-objective_func', type=str, default='log', help='log | sse | mse')
 
         # test
